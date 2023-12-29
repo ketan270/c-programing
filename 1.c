b1c11f2c9e839f80a03762c682f08b3f61a2d600
ketan270 <ketansharma2005@gmail.com>
2023-12-29 21:45:10 +0530

    C practice

---
+++ 1.c
@@ -0,0 +1,8 @@
+# include<stdio.h>
+int main() {
+   float side;
+   printf("ENTER SIDE :");
+   scanf(" %f",&side);
+   printf("AREA IS : %f",side*4);
+   return 0;
+}
---
+++ 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    float side;
+    printf("ENTER RADIUS :");
+    scanf("%f",&side);
+    printf("AREA OF CIRCLE : %f",3.14*side*side);
+    return 0;
+    }
