f47b5d57f07a18ad6cb2f4f71dc129cb9f0cfde7
ketan270 <ketansharma2005@gmail.com>
2023-12-30 20:11:32 +0530

    C Practice

--- 3.c
+++ 3.c
@@ -5,6 +5,7 @@ int main(){
     int b,c;
     b=c=3;
     int a= b + c;
+    printf("%d \n",a);
     int power = pow(b,c);
     printf("%d",power);
     return 0;
---
+++ 4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    // int a= 5 * (4/2) + 10;
+    // printf("%d",a);
+    // return 0;
+    printf("%d", 4>=5);
+}
