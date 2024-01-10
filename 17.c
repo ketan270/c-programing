09447b9f84320590372eea66f2cc714045cbbe0d
ketan270 <ketansharma2005@gmail.com>
2024-01-10 15:34:14 +0530

    printing first n natural numbers and their reverse

--- 17.c
+++ 17.c
@@ -5,8 +5,9 @@ int main (){
     printf("ENTER NUMBER: ");
     scanf("%d",&x);
     int sum = 0;
-    for(int i=1;i<=x;i++){
+    for(int i=1,j=x;i<=x && j>=1;i++,j--){
         sum=sum + i;
+        printf("%d\n",j);
     } 
     printf("SUM IS %d \n",sum);
     return 0;
