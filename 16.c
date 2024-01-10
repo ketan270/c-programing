2d127bc1a5e10dc0ba2c84c7c0e0bd9d23249048
ketan270 <ketansharma2005@gmail.com>
2024-01-10 14:44:22 +0530

    Using while and for loop to print numbers

---
+++ 16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+    int x;
+    printf("ENTER NUMBER: ");
+    scanf("%d",&x);
+    // int i=0;
+    //  while (i<=x){
+    //      printf("%d\n",i);
+    //      i++;
+    // }
+    for (int i=0;i<=x;i++){
+    printf("%d\n",i);
+    }
+    return 0;
+}
