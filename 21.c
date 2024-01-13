6a25e1deb63a82bc8de86b2b043e6d38270b960f
ketan270 <ketansharma2005@gmail.com>
2024-01-13 22:25:42 +0530

    to get factorial of any number

---
+++ 21.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+    for (int i=5;i<=50;i++){
+        if (i % 2 !=0){
+            printf("%d\n",i);
+        }
+    }    
+      return 0;
+}    
---
+++ 23.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n;
+    printf("ENTER NUMBER : ");
+    scanf("%d",&n);
+
+    int fact=1;
+    for (int i=1;i<=n;i++){
+        fact = fact * i;
+    }
+    printf("FACTORIAL IS %d",fact);
+    return 0;
+}
