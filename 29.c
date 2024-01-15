d43aa28f821ba643f1f4834fc7ddd9b1cf6f53b3
ketan270 <ketansharma2005@gmail.com>
2024-01-15 12:52:28 +0530

    Using if else if with function

---
+++ 29.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+void namaste();
+void bonjour();
+int main (){
+    printf("ENTER i for indian & f for french : ");
+    char ch;
+    scanf("%c",&ch);
+    if (ch == 'i'){
+    namaste();
+    }
+    else if (ch == 'f')
+     {
+        bonjour();
+    }
+    else{
+        printf("INVALID CHARACTER");
+    }
+    return 0;
+}
+void namaste(){
+    printf("NAMASTE");
+}
+void bonjour(){
+    printf("BONJOUR");
+}
