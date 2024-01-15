a08f14b920713dfa2611fb5e4a29916d400fac3e
ketan270 <ketansharma2005@gmail.com>
2024-01-15 12:23:52 +0530

    Using functions in C

---
+++ 27.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void printHello();
+
+int main(){
+    printHello();
+    printHello();
+    printHello();
+    return 0;
+}
+void printHello(){
+    printf("HELLO!\n");
+}
