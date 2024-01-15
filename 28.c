5edb456810e64f3d9c90b951b82934b08a58ef46
ketan270 <ketansharma2005@gmail.com>
2024-01-15 12:35:15 +0530

    using multiple functions

---
+++ 28.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+void Hello();
+void Good_bye();
+int main (){
+ Hello();
+ Good_bye();
+ return 0;
+}
+void Hello(){
+    printf("HELLO!\n");
+}
+void Good_bye(){
+    printf("GOOD BYE !");
+}
