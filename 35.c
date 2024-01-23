33a86c3e7daed975378e138ccae0029b408f2a09
ketan270 <ketansharma2005@gmail.com>
2024-01-23 21:56:41 +0530

    Using Recursion to Print sum of first n natural number

---
+++ 35.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int sum(int n);
+int main (){
+    int n;
+    printf("Enter Number: ");
+    scanf("%d",&n);
+    printf("Sum is %d",sum(n));
+    return 0;
+}
+int sum(int n){
+    if (n==1){
+        return 1;
+    }
+    int sumNm1=sum(n-1);
+    int sum1=sumNm1 +n;
+    return sum1;
+}
