01a40182e25cba2ad1e6fbefad7ef3fae0755a75
ketan270 <ketansharma2005@gmail.com>
2024-01-24 13:43:35 +0530

    Using Recursion to convert celsius to fahrenheit

--- 37.c
+++ 37.c
@@ -2,11 +2,11 @@
 #include<math.h>
 float temp(float celsius);
 int main(){
-    float far = temp(16);
+    float far = temp(2);
     printf("Fah is %f",far);
     return 0;
 }
 float temp(float celsius){
-    float far = celsius * (9/5) + 32;
+    float far = celsius * (9.0/5.0) + 32;
     return far;
 }
