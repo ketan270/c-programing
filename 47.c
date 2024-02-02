a44a154ce0452be930f14134b75969c53e26766f
ketan270 <ketansharma2005@gmail.com>
2024-02-02 21:46:29 +0530

    Ternary operator

---
+++ 47.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("ENTER NUMBER A: ");
+    scanf("%d",&a);
+    printf("ENTER NUMBER B: ");
+    scanf("%d",&b);
+    (a>b)? printf("A IS GREATER") : printf("B IS GREATER");
+
+}
