4ef7bed720e0f7847d5375df0ec796a8363e9ee5
ketan270 <ketansharma2005@gmail.com>
2024-02-04 19:09:00 +0530

    String mirror - Double strings

--- 45.c
+++ 45.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
+// #include<stdio.h>
+// #include<math.h>
+// #include<string.h>
+// int main (){
+//     char txt[] = "NUMEROUS TREE";
+//     printf("NUMBER OF CHARACTER IS %d", strlen(txt));
+//     return 0;
+// }
+#include <stdio.h>
 #include<math.h>
-#include<string.h>
-int main (){
-    char txt[] = "NUMEROUS TREE";
-    printf("NUMBER OF CHARACTER IS %d", strlen(txt));
-    return 0;
+int main() {
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d",&b);
+    int c= a/b;
+    printf("%d\n", c);
+    
 }
