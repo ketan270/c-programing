35fd1c884bdcafce4745eddf83b95aee3de917c8
ketan270 <ketansharma2005@gmail.com>
2024-02-15 20:18:45 +0530

    Storing data in 2d arrays

---
+++ 51.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int marks[2][3];
+    marks[0][0]=89;
+    marks[0][1]=90;
+    marks[0][2]=91;
+
+    marks[1][0]=92;
+    marks[1][1]=93;
+    marks[1][2]=94;
+    printf("%d ",marks[0][1]);
+    return 0;
+}
