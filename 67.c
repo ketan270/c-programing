0dbe20dc06489ae4af45aa968f9091acbce8cbe5
Ketan270 <ketansharma2005@gmail.com>
2024-02-28 21:23:05 +0530

    Making Calculator using C language

--- 67.c
+++ 67.c
@@ -1,27 +1,21 @@
 #include<stdio.h>
 #include<math.h>
-int main(){
-    int a,b;
-    scanf("%d\n%d",&a,&b);
+
+int main() {
+    int a, b;
+    scanf("%d\n%d", &a, &b);
     char operator;
     printf("ENTER OPERATOR: ");
-    scanf("%c",&operator);
-    if (operator =='+')
-    {
-        printf("%d",a+b);
-    }
-    else if (operator=='-')
-    {
-        printf("%d",a-b);
-    }
-    else if (operator=='/')
-    {
-        printf("%d",a/b);
-    }
-    else if (operator=='*')
-    {
-        printf("%d",a*b);
-    }
+    scanf(" %c", &operator);
     
+    if (operator == '+') {
+        printf("%d", a + b);
+    } else if (operator == '-') {
+        printf("%d", a - b);
+    } else if (operator == '/') {
+        printf("%d", a / b);
+    } else if (operator == '*') {
+        printf("%d", a * b);
+    }
     return 0;
 }
