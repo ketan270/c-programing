c97db27169c1e9832a196f8233509153ba8ce977
Ketan270 <ketansharma2005@gmail.com>
2024-03-02 18:52:43 +0530

    PATTERN IN

---
+++ 72.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+    int i,j;
+    for (i = 5; i >=1; i--)
+    {
+        for (j = 5;j>=1; j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
