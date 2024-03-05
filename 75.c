4db5ea287b22efb67ab247b1d91491eaf30f56f8
Ketan270 <ketansharma2005@gmail.com>
2024-03-05 19:44:32 +0530

    Pattern Question

---
+++ 75.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i,j;
+    for (i = 1; i <=5; i++)
+    {
+        for ( j = i; j>=1; j--)
+        {
+            printf("%c ",j+64);
+        }
+        printf("\n");
+        
+    }
+    return 0;
+    
+}
