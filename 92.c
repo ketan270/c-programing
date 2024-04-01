64f45ebfc3399063db86bebf9046f5212ea06dd0
Ketan270 <ketansharma2005@gmail.com>
2024-04-01 21:27:01 +0530

    a & b are written in a file replace them with their sum

---
+++ 92.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    FILE *fptr;
+    fptr=fopen("Sum.txt","r");
+    int a;
+    fscanf(fptr,"%d\t",&a);
+    int b;
+    fscanf(fptr,"%d\t",&b);
+    fclose(fptr);
+    fptr=fopen("Sum.txt","w");
+    fprintf(fptr,"%d",a+b);
+    fclose(fptr);
+    return 0;
+}
